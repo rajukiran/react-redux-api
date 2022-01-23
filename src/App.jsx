8b706d81c737a9c6c64b87b9e00496f51ef9a8d6
kiran raju <kiranraju@kirans-MacBook-Pro.local>
2022-01-23 14:16:58 +0530

    moved components to proper files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import List from "./List";
-import Form from "./js/components/Form";
+import List from "./components/List";
+import Form from "./components/Form";
 
 const App = () => (
   <>
--- src/AppContainer.jsx
+++ src/AppContainer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import App from "./App";
-import { store, persistor } from "./js/store";
+import { store, persistor } from "./state/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
 
